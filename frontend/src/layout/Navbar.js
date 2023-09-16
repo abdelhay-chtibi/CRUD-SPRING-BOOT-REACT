0f3b69b637e8a4cdcb19474833fadf81c650ee83
abdelhay-chtibi <142673856+abdelhay-chtibi@users.noreply.github.com>
2023-09-16 11:56:11 +0100

    front

---
+++ frontend/src/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">Application CRUD</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <Link to={'/adduser'}>
+                        <button className='btn btn-outline-dark' >Add User</button>
+                    </Link>
+
+
+
+                </div>
+            </nav>
+        </div>
+    )
+}
